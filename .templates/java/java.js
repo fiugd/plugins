c14d8162ce9aa0bb60113cd7ddfd9a6eac01d79a
crosshj <github@chimpjuice.com>
2023-07-24 14:30:13 -0400

    -m java template

--- .templates/java/java.js
+++ .templates/java/java.js
@@ -5,3 +5,21 @@
 	todo: https://github.com/plasma-umass/doppio/tree/master/docs
 
 */
+
+const outputEl = document.querySelector('pre');
+
+(async () => {
+	const output = `
+		WIP:
+		see https://github.com/plasma-umass/doppio/tree/master/docs
+		supports Java 8 SDK
+			- java
+			- scala
+			- kotlin?
+			- groovy?
+			- clojure?
+			- frege? (mostly compliant with Haskell)
+	`.trim().replace(/^\t\t/gm, '');
+
+	outputEl.textContent = output;
+})();
