bbf6ab072cbb4a67a4ef792e45be655584fa83d9
crosshj <github@chimpjuice.com>
2023-07-24 12:07:29 -0400

    -m better scheme example

--- languages/scheme.scm
+++ languages/scheme.scm
@@ -1,24 +1,52 @@
-; scheme loves parenthesis tails
+; https://www.biwascheme.org/doc/reference.html
+; https://groups.csail.mit.edu/mac/ftpdir/scheme-7.4/doc-html/scheme_2.html
 
 (define range
 	(lambda (n . m)
 		(let
-			((n (if (null? m) 0 n)) (m (if (null? m) n (car m))))
+			((n (if (null? m) 0 n)) (m (if (null? m) n (car m)))
+		)
 			(cond
-		((= n m) (list n))
-		(else (cons n (range ((if (< n m) + -) n 1) m)))))))
+				((= n m) (list n))
+				(else
+					(cons
+						n
+						(range
+							((if (< n m) + -) n 1) m)
+						)
+					)
+				)
+			)
+	)
+)
 
 (define (fib n)
 	(cond
 		((= n 0) 0)
 		((= n 1) 1)
 		(else
-		(+ (fib (- n 1))
-				(fib (- n 2)))))
+			(+ (fib (- n 1))
+				(fib (- n 2))
+			)
+		)
+	)
 )
 
 (define (fibMany numbers)
 	(if (null? numbers) '()
-	(cons (fib (car numbers)) (fibMany (cdr numbers)))))
+		(cons
+			(string-append (number->string 
+				(fib (car numbers))
+				10
+			) " ")
+			(fibMany (cdr numbers))
+		)
+	)
+)
 
-(fibMany (range 9))
+(apply
+	string-append
+	(fibMany (range 9))
+)
+
+
