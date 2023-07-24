988ea20a0343e62f9c31c6bd7e9abd7ef1ed7b14
crosshj <github@chimpjuice.com>
2023-07-24 13:59:45 -0400

    -m java template

--- .templates/java/java.js
+++ .templates/java/java.js
@@ -1,1 +1,7 @@
-/* todo: https://github.com/plasma-umass/doppio/tree/master/docs */
+/*
+
+	use?: http://teavm.org/sandbox/
+	use?: https://plasma-umass.org/doppio-demo/
+	todo: https://github.com/plasma-umass/doppio/tree/master/docs
+
+*/
