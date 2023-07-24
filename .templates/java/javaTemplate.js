926ae935c52201fd03e27779e7320eb825032f85
crosshj <github@chimpjuice.com>
2023-07-24 16:19:39 -0400

    -m java template

--- .templates/java/javaTemplate.js
+++ .templates/java/javaTemplate.js
@@ -7,6 +7,24 @@
 const inputEl = document.querySelector('textarea.input')
 const outputEl = document.querySelector('pre.output');
 
+// Wrap in a closure; don't pollute the global namespace.
+const setupBrowserFS = () => {
+	const mfs = new BrowserFS.FileSystem.MountableFileSystem();
+	const fs = BrowserFS.BFSRequire('fs');
+
+	BrowserFS.initialize(mfs);
+	mfs.mount('/tmp', new BrowserFS.FileSystem.InMemory());
+	mfs.mount('/home', new BrowserFS.FileSystem.LocalStorage());
+	// The first argument is the filename of the listings file
+	// The second argument is the relative URL to the folder containing the listings file
+	// and the data it indexes.
+	// In this example, the listings file and DoppioJVM's data is at
+	// <thiswebpage>/doppio/listings.json
+	mfs.mount('/sys', new BrowserFS.FileSystem.XmlHttpRequest('listings.json'));
+};
+
+
+
 (async () => {
 	const code = inputEl.textContent;
 	console.log({ code })
@@ -23,5 +41,7 @@ const outputEl = document.querySelector('pre.output');
 	`.trim().replace(/^\t\t/gm, '');
 
 	outputEl.textContent = output;
+	
+	setupBrowserFS();
 })();
 
